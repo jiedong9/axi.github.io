e65ee98fc6f5c6951694d5c12f11bf514d8d7fff
Axi <jiedong9@gmail.com>
2019-09-29 09:20:11 +0800

    完成

--- jianli_demo/main.js
+++ jianli_demo/main.js
@@ -1,6 +1,6 @@
 let html = document.querySelector('#html')
 let style = document.querySelector('#style')
-let string = `/* 你好，我叫小方
+let string = `/* 你好，我叫axi
 * 接下来我演示一下我的前端功底
 * 首先我要准备一个div
 **/
@@ -8,14 +8,49 @@ let string = `/* 你好，我叫小方
     border: 1px solid green;
     width: 200px;
     height: 200px;
-    
 }
 
-已成功部署，无需安装，请登录修改默认账号密码
+/* 接下来我把 div 变成一个八卦图
+ * 注意看好了
+ * 首先，把 div 变成一个圆
+ **/
 
-用户：admin
+#div1{
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border: none;
+}
 
-密码：zblogger
+/* 八卦是阴阳形成的
+ * 一黑一白
+ **/
+
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+
+/* 加两个小球 */
+#div1::before{
+    width: 100px;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #000;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#div1::after{
+    width: 100px;
+    height: 100px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background: #fff;
+    border-radius: 50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
+}
+/* 完 */
 `
 
 let string2 = ''
@@ -24,7 +59,7 @@ let n = 0
 let step = () => {
 
     setTimeout(() => {
-
+        // 处理换行和空格
         if (string[n] === '\n') {
             string2 += '<br>'
         } else if (string[n] === ' ') {
@@ -33,15 +68,16 @@ let step = () => {
             string2 += string[n]
         }
         html.innerHTML = string2
-        style.innerHTML = string.substring(0, n)
+        style.innerHTML = string.substring(0, n) //将其用样式文本打印出来
+        // 在最后自动滚动
+        window.scrollTo(0, 88888)
+        html.scrollTo(0, 88888)
         if (n < string.length - 1) {
             n += 1
             step()
         }
 
-    }, 100)
-
-
+    }, 10)
 
 }
 
